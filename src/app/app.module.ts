c442cef02778e0a1dfabf0d4068765f0bd6a8bfa
Evan Liomain <evan.liomain@gmail.com>
2023-04-07 13:23:31 +0200

    🎉 Initialize the repo with certification source code + some custom configuration

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameResultsComponent } from './game-results/game-results.component';
+import { GameStatsComponent } from './game-stats/game-stats.component';
+import { TeamStatsComponent } from './team-stats/team-stats.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeamStatsComponent,
+    GameResultsComponent,
+    GameStatsComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
